c858f00f394f6e503b639bb75fa89ffd1b1e7c6d
Shawn R Petros <SPETROS@amfam.com>
2021-06-10 16:42:38 -0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Shawn Petros on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
